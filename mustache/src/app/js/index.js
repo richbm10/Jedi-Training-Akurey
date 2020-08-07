3c3e3c3efbc8893045161127e74441a795604c60
richbm10 <ricardobonilla99@hotmail.com>
2020-08-07 15:31:41 -0600

    [CLOSE-mustache]

--- mustache/src/app/js/index.js
+++ mustache/src/app/js/index.js
@@ -40,53 +40,12 @@ function filterList(contractServiceCanvas, searchTerm) {
     });
 }
 
-function createContractServiceHeader(company) {
-    const header = document.createElement('div');
-    header.classList.add('contract-service__header', 'flex-container-row-A');
-    const h = document.createElement('h2');
-    h.classList.add('text-A');
-    h.textContent = company.name;
-    header.appendChild(h);
-    if (company.isCertified) {
-        const img = document.createElement('img');
-        img.classList.add('contract-service__icon');
-        img.src = './assets/icons/certified.png';
-        header.appendChild(img);
-    }
-    return header;
-}
-
-function createContractService(company) {
-    const listItem = document.createElement('li');
-    listItem.classList.add('contract-service');
-    const contractServiceHeader = createContractServiceHeader(company);
-    const line = document.createElement('div');
-    line.classList.add('contract-service__line');
-    const img = document.createElement('img');
-    img.classList.add('contract-service__img');
-    img.src = company.image;
-    const p = document.createElement('p');
-    p.classList.add('contract-service__text', 'text-C');
-    p.textContent = company.description;
-    const span = document.createElement('span');
-    span.classList.add('contract-service__span', 'text-B');
-    span.textContent = `Desde: $${company.rate}/${company.hours} horas`;
-    const button = document.createElement('div');
-    button.classList.add('button', 'text-B');
-    button.textContent = 'Contratar';
-    listItem.appendChild(contractServiceHeader);
-    listItem.appendChild(line);
-    listItem.appendChild(img);
-    listItem.appendChild(p);
-    listItem.appendChild(span);
-    listItem.appendChild(button);
-    return listItem;
-}
 
 function main() {
     const contractServiceCanvas = document.querySelector('#contract-service-canvas');
     const heroSectionInput = document.querySelector('#hero-section__input input');
     const heroSectionIcon = document.querySelector('#hero-section__icon-img');
+    const template = document.querySelector('#contract-service-template').innerHTML;
 
     heroSectionIcon.addEventListener('click', (event) => {
         filterList(contractServiceCanvas, heroSectionInput.value);
@@ -98,7 +57,7 @@ function main() {
 
     services.getCompanies().then(companies => {
         companies.forEach(company => {
-            contractServiceCanvas.appendChild(createContractService(company));
+            contractServiceCanvas.insertAdjacentHTML('beforeend', Mustache.render(template, company));
         });
     }).catch(err => {
         alert(err);
